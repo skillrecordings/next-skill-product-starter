3fade7b9f2e4842bcc8407ff35a2589936651733
Vojta Holik <vojta@egghead.io>
2021-04-20 15:16:57 +0200

    feat: add types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,70 @@
-import Layout from 'layouts'
-import Link from 'next/link'
+import * as React from 'react'
 import ConvertkitSubscribeForm from 'components/forms/convertkit'
+import Link from 'next/link'
+import Layout from 'layouts'
 
-const index = [
+export default function Home() {
+  return (
+    <Layout>
+      <div className="space-y-16">
+        {content.map((section) => {
+          return (
+            <section className="pt-16">
+              <h2 className="uppercase font-semibold tracking-wide text-sm pb-4 dark:text-gray-300 text-gray-600">
+                {section.label}
+              </h2>
+              <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
+                {section?.items?.map((item: Item) => {
+                  if (item.link) {
+                    return (
+                      <Link href={item.link}>
+                        <a className="rounded-lg border-2 dark:hover:bg-gray-900 hover:bg-gray-100 dark:border-gray-900 border-gray-100 dark:bg-black bg-white text-3xl font-extrabold tracking-tight py-24 flex items-center justify-center flex-col transition-colors duration-100 ease-in-out min-h-[300px]">
+                          {item.label}
+                        </a>
+                      </Link>
+                    )
+                  }
+                  if (item.items) {
+                    return (
+                      <div className="rounded-lg border-2 dark:border-gray-900 border-gray-100 dark:bg-black bg-white text-2xl font-extrabold tracking-tight py-16 flex items-center justify-center flex-col transition-colors duration-100 ease-in-out min-h-[300px]">
+                        <div className="text-3xl">{item?.label}</div>
+                        {item.items.map((item: any) => (
+                          <Link href={item.link}>
+                            <a className="dark:hover:text-rose-300 hover:text-rose-500 font-semibold">
+                              {item.label}
+                            </a>
+                          </Link>
+                        ))}
+                      </div>
+                    )
+                  }
+                  if (item.component) {
+                    return (
+                      <div className="sm:p-8 p-4 border-2 dark:border-gray-900 border-gray-100 dark:bg-black bg-white rounded-lg transition-colors duration-100 ease-in-out min-h-[300px]">
+                        {React.cloneElement(item.component, {
+                          children: item.label,
+                        })}
+                      </div>
+                    )
+                  }
+                })}
+              </div>
+            </section>
+          )
+        })}
+      </div>
+    </Layout>
+  )
+}
+
+type Item = {
+  label: string
+  link?: string
+  items?: Item[]
+  component?: React.ReactElement
+}
+
+const content: Item[] = [
   {
     label: 'templates',
     items: [
@@ -20,7 +82,7 @@ const index = [
     label: 'pages',
     items: [
       {
-        label: '',
+        label: 'Newsletter',
         items: [
           {
             label: 'Confirm',
@@ -62,64 +124,15 @@ const index = [
     label: 'components',
     items: [
       {
+        label: 'Subscribe Form',
         component: (
           <ConvertkitSubscribeForm
             onSubmit={() => {
               window.alert('Configure me!')
             }}
-          >
-            <div className="pb-4 font-bold sm:text-xl text-lg tracking-tight">
-              Subscribe Form
-            </div>
-          </ConvertkitSubscribeForm>
+          />
         ),
       },
     ],
   },
 ]
-
-export default function Home() {
-  return (
-    <Layout>
-      <div className="space-y-16">
-        {index.map((section) => {
-          return (
-            <section className="pt-16">
-              <h2 className="uppercase font-semibold tracking-wide text-sm pb-4 dark:text-gray-300 text-gray-600">
-                {section.label}
-              </h2>
-              <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
-                {section?.items?.map((item: any) => {
-                  return (
-                    (item.link && (
-                      <Link href={item.link}>
-                        <a className="rounded-lg border-2 dark:hover:bg-gray-900 hover:bg-gray-100 dark:border-gray-900 border-gray-100 dark:bg-black bg-white text-3xl font-extrabold tracking-tight py-24 flex items-center justify-center flex-col transition-colors duration-100 ease-in-out">
-                          {item.label}
-                        </a>
-                      </Link>
-                    )) ||
-                    (item.items && (
-                      <div className="rounded-lg border-2 dark:border-gray-900 border-gray-100 dark:bg-black bg-white text-3xl font-extrabold tracking-tight py-24 flex items-center justify-center flex-col transition-colors duration-100 ease-in-out">
-                        {item.items.map((item: any) => (
-                          <Link href={item.link}>
-                            <a className="dark:hover:text-rose-300 hover:text-rose-500">
-                              {item.label}
-                            </a>
-                          </Link>
-                        ))}
-                      </div>
-                    )) || (
-                      <div className="sm:p-8 p-4 border-2 dark:border-gray-900 border-gray-100 dark:bg-black bg-white rounded-lg transition-colors duration-100 ease-in-out">
-                        {item.component}
-                      </div>
-                    )
-                  )
-                })}
-              </div>
-            </section>
-          )
-        })}
-      </div>
-    </Layout>
-  )
-}
