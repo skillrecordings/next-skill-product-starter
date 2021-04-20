39c3d3e94c4cea6ea2ba824481013a728b2f23d5
Vojta Holik <vojta@egghead.io>
2021-04-20 14:51:49 +0200

    refactor: use array to define contents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,94 +2,123 @@ import Layout from 'layouts'
 import Link from 'next/link'
 import ConvertkitSubscribeForm from 'components/forms/convertkit'
 
+const index = [
+  {
+    label: 'templates',
+    items: [
+      {
+        label: 'Article',
+        link: '/article',
+      },
+      {
+        label: 'Video',
+        link: '/video',
+      },
+    ],
+  },
+  {
+    label: 'pages',
+    items: [
+      {
+        label: '',
+        items: [
+          {
+            label: 'Confirm',
+            link: '/confirm',
+          },
+          {
+            label: 'Confirmed',
+            link: '/confirmed',
+          },
+          {
+            label: 'Excited',
+            link: '/excited',
+          },
+          {
+            label: 'Unsubscribed',
+            link: '/unsubscribed',
+          },
+        ],
+      },
+      {
+        label: 'Quiz',
+        link: '/answer?question=interviews',
+      },
+      {
+        label: 'Login',
+        link: '/login',
+      },
+      {
+        label: 'Thanks',
+        link: '/thanks?email=example@email.com',
+      },
+      {
+        label: 'Buy',
+        link: '/buy',
+      },
+    ],
+  },
+  {
+    label: 'components',
+    items: [
+      {
+        component: (
+          <ConvertkitSubscribeForm
+            onSubmit={() => {
+              window.alert('Configure me!')
+            }}
+          >
+            <div className="pb-4 font-bold sm:text-xl text-lg tracking-tight">
+              Subscribe Form
+            </div>
+          </ConvertkitSubscribeForm>
+        ),
+      },
+    ],
+  },
+]
+
 export default function Home() {
   return (
     <Layout>
       <div className="space-y-16">
-        <section className="pt-16">
-          <h2 className="uppercase font-semibold tracking-wide text-sm pb-4 dark:text-gray-300 text-gray-600">
-            Templates
-          </h2>
-          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
-            <Link href="/article">
-              <a className="rounded-lg border-2 dark:hover:bg-gray-900 hover:bg-gray-100 dark:border-gray-900 border-gray-100 dark:bg-black bg-white text-3xl font-extrabold tracking-tight py-24 flex items-center justify-center flex-col transition-colors duration-100 ease-in-out">
-                Article
-              </a>
-            </Link>
-            <Link href="/video">
-              <a className="rounded-lg border-2 dark:hover:bg-gray-900 hover:bg-gray-100 dark:border-gray-900 border-gray-100 dark:bg-black bg-white text-3xl font-extrabold tracking-tight py-24 flex items-center justify-center flex-col transition-colors duration-100 ease-in-out">
-                Video
-              </a>
-            </Link>
-          </div>
-        </section>
-        <section>
-          <h2 className="uppercase font-semibold tracking-wide text-sm pb-4 dark:text-gray-300 text-gray-600">
-            Pages
-          </h2>
-          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
-            <div className="rounded-lg border-2 dark:border-gray-900 border-gray-100 dark:bg-black bg-white text-3xl font-extrabold tracking-tight py-24 flex items-center justify-center flex-col transition-colors duration-100 ease-in-out">
-              <Link href="/confirm">
-                <a className="dark:hover:text-rose-300 hover:text-rose-500">
-                  Confirm
-                </a>
-              </Link>
-              <Link href="/confirmed">
-                <a className="dark:hover:text-rose-300 hover:text-rose-500">
-                  Confirmed
-                </a>
-              </Link>
-              <Link href="/excited">
-                <a className="dark:hover:text-rose-300 hover:text-rose-500">
-                  Excited
-                </a>
-              </Link>
-              <Link href="/unsubscribed">
-                <a className="dark:hover:text-rose-300 hover:text-rose-500">
-                  Unsubscribed
-                </a>
-              </Link>
-              <Link href="/login">
-                <a className="dark:hover:text-rose-300 hover:text-rose-500">
-                  Login
-                </a>
-              </Link>
-              <Link href="/thanks?email=example@team.com">
-                <a className="dark:hover:text-rose-300 hover:text-rose-500">
-                  Thanks
-                </a>
-              </Link>
-              <Link href="/buy">
-                <a className="dark:hover:text-rose-300 hover:text-rose-500">
-                  Buy
-                </a>
-              </Link>
-            </div>
-            <Link href="/answer?question=interviews">
-              <a className="rounded-lg border-2 dark:hover:bg-gray-900 hover:bg-gray-100 dark:border-gray-900 border-gray-100 dark:bg-black bg-white text-3xl font-extrabold tracking-tight py-24 flex items-center justify-center flex-col transition-colors duration-100 ease-in-out">
-                Answer (Quiz)
-              </a>
-            </Link>
-          </div>
-        </section>
-        <section>
-          <h2 className="uppercase font-semibold tracking-wide text-sm pb-4 dark:text-gray-300 text-gray-600">
-            Components
-          </h2>
-          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
-            <div className="sm:p-8 p-4 border-2 dark:border-gray-900 border-gray-100 dark:bg-black bg-white rounded-lg transition-colors duration-100 ease-in-out">
-              <ConvertkitSubscribeForm
-                onSubmit={() => {
-                  console.log('Configure me!')
-                }}
-              >
-                <div className="pb-4 font-bold sm:text-xl text-lg tracking-tight">
-                  Subscribe Form
-                </div>
-              </ConvertkitSubscribeForm>
-            </div>
-          </div>
-        </section>
+        {index.map((section) => {
+          return (
+            <section className="pt-16">
+              <h2 className="uppercase font-semibold tracking-wide text-sm pb-4 dark:text-gray-300 text-gray-600">
+                {section.label}
+              </h2>
+              <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
+                {section?.items?.map((item: any) => {
+                  return (
+                    (item.link && (
+                      <Link href={item.link}>
+                        <a className="rounded-lg border-2 dark:hover:bg-gray-900 hover:bg-gray-100 dark:border-gray-900 border-gray-100 dark:bg-black bg-white text-3xl font-extrabold tracking-tight py-24 flex items-center justify-center flex-col transition-colors duration-100 ease-in-out">
+                          {item.label}
+                        </a>
+                      </Link>
+                    )) ||
+                    (item.items && (
+                      <div className="rounded-lg border-2 dark:border-gray-900 border-gray-100 dark:bg-black bg-white text-3xl font-extrabold tracking-tight py-24 flex items-center justify-center flex-col transition-colors duration-100 ease-in-out">
+                        {item.items.map((item: any) => (
+                          <Link href={item.link}>
+                            <a className="dark:hover:text-rose-300 hover:text-rose-500">
+                              {item.label}
+                            </a>
+                          </Link>
+                        ))}
+                      </div>
+                    )) || (
+                      <div className="sm:p-8 p-4 border-2 dark:border-gray-900 border-gray-100 dark:bg-black bg-white rounded-lg transition-colors duration-100 ease-in-out">
+                        {item.component}
+                      </div>
+                    )
+                  )
+                })}
+              </div>
+            </section>
+          )
+        })}
       </div>
     </Layout>
   )
